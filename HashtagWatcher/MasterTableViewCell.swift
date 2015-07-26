cbe7a16e13d97a073b7f1430285ad6379746948f
shubhitms <shubhits@andrew.cmu.edu>
2015-07-25 17:45:33 -0700

    initial commit

---
+++ HashtagWatcher/MasterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MasterTableViewCell.swift
+//  HashtagWatcher
+//
+//  Created by Shubhit Singh on 7/24/15.
+//  Copyright (c) 2015 Shubhit Singh. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
